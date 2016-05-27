ef23d7d7cba7eeae8fd2d5a2210cae8efa2efd3d
ashish <ashishsharma303@hotmail.com>
2016-05-27 21:12:03 +0530

    RDS-related

---
+++ RDS-Related/getRDSCollection.ps1
@@ -0,0 +1,127 @@
+﻿param(
+  
+  $username = "mydomain\azureuser",
+  $Password = "Password@12345",
+  [String]$Broker = "Broker.Mydomain.local"
+
+) # /param  
+
+
+Enable-WSManCredSSP -Role Client -DelegateComputer * -Force
+$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+$vmsessionOption = New-PSSessionOption -SkipCACheck -SkipCNCheck
+$Session01 = new-PSSession -ComputerName "138.91.248.126" -Credential $Cred -UseSSL -SessionOption $vmsessionOption -Authentication Credssp
+
+
+Invoke-Command -Session $Session01 -ScriptBlock { param($Broker)
+
+Write-Output $Broker
+# checking if the RDS tools are already installed on local Machine.
+$localMachine = $env:COMPUTERNAME
+$RDSToolCheck = Get-WindowsFeature -Name "RSAT-RDS-Tools" -ComputerName $localMachine
+if ($RDSToolCheck.InstallState -ne "Installed")
+{
+        Install-WindowsFeature RSAT-RDS-Gateway,RSAT-RDS-Tools,RDS-Licensing-UI,RSAT-RDS-Licensing-Diagnosis-UI -Verbose
+}
+else{ Write-Output "Tools are already installed on localMachine $($localMachine)" }
+
+$getRDSModudle = Get-Module -Name RemoteDesktop -ErrorAction SilentlyContinue
+If($getRDSModudle){Write-Output "Remotedesktop Module is present on local Machine $($localMachine) `n"}Else{ Import-Module RemoteDesktop -ErrorAction SilentlyContinue }
+
+$getRdServer = Get-RDServer -ConnectionBroker $Broker
+If($getRdServer) {}else{Write-Output "The Remote Desktop Management service is not running on the RD Connection Broker server `n you might not provided the correct Broker server input or local machine has not the RDS tools configured." }
+
+function RoleCheck ($RoleValue)
+{
+    $ServerRole = New-Object System.Collections.ArrayList
+    foreach ($Server in $getRdServer)
+    {
+        $role = $null
+        foreach ($role in $Server.Roles)
+        {
+            If($role -contains $RoleValue)
+            {
+                $Server = $server.Server
+                if ($Server)
+                {
+                   $ServerRole.Add($Server) | Out-Null
+                }
+                
+            }
+        }
+    }
+return $ServerRole
+}
+
+$RDS_RD_Server = RoleCheck -RoleValue "RDS-RD-SERVER"
+$RDS_WEB_ACCESS = RoleCheck -RoleValue "RDS-WEB-ACCESS"
+$RDS_CONNECTION_BROKER = RoleCheck -RoleValue "RDS-CONNECTION-BROKER"
+$RDS_GATEWAY = RoleCheck -RoleValue "RDS-GATEWAY"
+$RDS_LICENSING = RoleCheck -RoleValue "RDS-LICENSING"
+
+# Extract of the information Ends here
+
+Write-Output "`n RD server : "$RDS_RD_Server
+Write-Output "`n RDS_WEB_ACCESS : "$RDS_WEB_ACCESS
+
+# Getting Session Collection information
+$getRdsSessionCollection = Get-RDSessionCollection -ConnectionBroker "Broker.mydomain.local"
+if ($getRdsSessionCollection)
+{
+    $CollectionArray = New-Object System.Collections.ArrayList
+    foreach ($Collection in $getRdsSessionCollection)
+    {
+        try
+        {
+            $GetUserGroup = Get-RDSessionCollectionConfiguration -CollectionName $Collection.CollectionName -UserGroup -ConnectionBroker $RDS_CONNECTION_BROKER -ErrorAction SilentlyContinue
+            $GetConnection = Get-RDSessionCollectionConfiguration -CollectionName $Collection.CollectionName -Connection -ConnectionBroker $RDS_CONNECTION_BROKER -ErrorAction SilentlyContinue
+            $GetUserProfileDisk = Get-RDSessionCollectionConfiguration -CollectionName $Collection.CollectionName -UserProfileDisk -ConnectionBroker $RDS_CONNECTION_BROKER -ErrorAction SilentlyContinue
+            $GetLoadbalancing = Get-RDSessionCollectionConfiguration -CollectionName $Collection.CollectionName -LoadBalancing -ConnectionBroker $RDS_CONNECTION_BROKER -ErrorAction SilentlyContinue
+            $GetClient = Get-RDSessionCollectionConfiguration -CollectionName $Collection.CollectionName -Client -ConnectionBroker $RDS_CONNECTION_BROKER -ErrorAction SilentlyContinue
+            $GetSecurity = Get-RDSessionCollectionConfiguration -CollectionName $Collection.CollectionName -Security -ConnectionBroker $RDS_CONNECTION_BROKER -ErrorAction SilentlyContinue
+
+            $CollectionGroup = New-Object PSObject -Property @{
+            CollectionName = $Collection.CollectionName;
+            Collection_ResourceType = $Collection.ResourceType;
+            Collection_Size = $Collection.Size;
+            Collection_Alias = $Collection.CollectionAlias;
+            Collection_UserGroup = $GetUserGroup.UserGroup;
+            Connection_DisconnectedSessionLimitMin = $GetConnection.DisconnectedSessionLimitMin;
+            Connection_ActiveSessionLimitMin = $GetConnection.ActiveSessionLimitMin;
+            Connection_IdleSessionLimitMin = $GetConnection.IdleSessionLimitMin;
+            Connection_TemporaryFoldersDeletedOnExit = $GetConnection.TemporaryFoldersDeletedOnExit;
+            Connection_AutomaticReconnectionEnabled = $GetConnection.AutomaticReconnectionEnabled;
+            UserProfileDisk_DiskPath = $GetUserProfileDisk.DiskPath;
+            UserProfileDisk_EnableUserProfileDisk = $GetUserProfileDisk.EnableUserProfileDisk;
+            UserProfileDisk_MaxUserProfileDiskSizeGB = $GetUserProfileDisk.MaxUserProfileDiskSizeGB;
+            LoadBalancing_SessionLimit = $GetLoadbalancing.SessionLimit;
+            LoadBalancing_SessionHost = $GetLoadbalancing.SessionHost;
+            Client_MaxRedirectedMonitors = $GetClient.MaxRedirectedMonitors;
+            Client_ClientDeviceRedirectionOptions = $GetClient.ClientDeviceRedirectionOptions;
+            Security_AuthenticateUsingNLA = $GetSecurity.AuthenticateUsingNLA;
+            Security_EncryptionLevel = $GetSecurity.EncryptionLevel;
+            Security_SecurityLayer = $GetSecurity.SecurityLayer;
+
+            } # end of PSObject
+        }
+        catch [System.Exception]
+        {
+            Write-Verbose $Error[0].ToString()
+        }
+
+        $CollectionArray.Add($CollectionGroup) | Out-Null
+    }
+
+$CollectionArray | Out-GridView
+} # end of RDS collection code.
+
+
+} -ArgumentList $Broker # end of Invoke command Script Block  
+
+
+Get-PSSession | Remove-PSSession -Verbose
+
+
+
+
