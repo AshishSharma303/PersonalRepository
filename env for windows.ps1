2128c925610184afc199c8e0e52310f6737cc169
AshishSharma303 <ashishsharma303@hotmail.com>
2022-04-07 11:07:44 -0500

    Create env for windows.ps1

---
+++ env for windows.ps1
@@ -0,0 +1,18 @@
+# Specify the OSM version that will be leveraged throughout these instructions
+$OSM_VERSION="v1.0.0"
+
+[Net.ServicePointManager]::SecurityProtocol = "tls12"
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -URI "https://github.com/openservicemesh/osm/releases/download/$OSM_VERSION/osm-$OSM_VERSION-windows-amd64.zip" -OutFile "osm-$OSM_VERSION.zip"
+Expand-Archive -Path "osm-$OSM_VERSION.zip" -DestinationPath .
+
+
+# Copy osm.exe to C:\OSM
+New-Item -ItemType Directory -Force -Path "C:\OSM"
+Move-Item -Path .\windows-amd64\osm.exe -Destination "C:\OSM\"
+
+# Add C:\OSM to PATH.
+# Make the new PATH permanently available for the current User
+$USER_PATH = [environment]::GetEnvironmentVariable("PATH", "User") + ";C:\OSM\"
+[environment]::SetEnvironmentVariable("PATH", $USER_PATH, "User")
+# Make the new PATH immediately available in the current shell
+$env:PATH += ";C:\OSM\"
